b616d8ce79bbc0a20f8460a60f4fcaa57a4ce8ae
rtspw <richard.cx.tang@gmail.com>
2019-11-14 00:52:06 -0800

    Updates

---
+++ precache-manifest.70017ae3dc2af8ab9c468001b21f0767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c65b4a197de2c7786d7bd6944609b8",
+    "url": "/Karnaugh-Maps-Revisited/index.html"
+  },
+  {
+    "revision": "432f9317285d4efb7263",
+    "url": "/Karnaugh-Maps-Revisited/static/css/main.cc27e018.chunk.css"
+  },
+  {
+    "revision": "1821dedb374055228673",
+    "url": "/Karnaugh-Maps-Revisited/static/js/2.a24bd8c3.chunk.js"
+  },
+  {
+    "revision": "432f9317285d4efb7263",
+    "url": "/Karnaugh-Maps-Revisited/static/js/main.eee0ab14.chunk.js"
+  },
+  {
+    "revision": "b5fbcc295f6c5869fc9e",
+    "url": "/Karnaugh-Maps-Revisited/static/js/runtime~main.7a311729.js"
+  }
+]);
