7de1db74685fb761312cb38661351e94e2b40eb9
rtspw <richard.cx.tang@gmail.com>
2019-11-14 00:26:56 -0800

    Minor code changes and comments in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ function App() {
     document.title = `${varNumPage} Variable K-Map Visual`;
   });
 
+  /* Changes which table is displayed */
   function handlePanelClick(varNum) {
     setVarNumPage(varNum);
     const newGridValues = new Array(Math.pow(2, varNum)).fill().map(() => new GridButtonData());
@@ -38,12 +39,16 @@ function App() {
     setMintermGroupings([]);
   }
 
+  /* Resets the table completely with blank grid values */
   function handleClearButtonClick() {
     setGridValues(new Array(Math.pow(2, varNumPage)).fill().map(() => new GridButtonData()));
     setMintermGroupings([]);
     setSelectedMintermGroup(0);
   }
 
+  /* When there is more than one possible solution detected, a next button will 
+   * appear next to the group display box. Clicking this button cycles to the
+   * next solution and updates the colors of the grid values */
   function handleNextGroupingButtonClick() {
     let next = (() => {
       if (selectedMintermGroup + 1 >= mintermGroupings.length) return 0;
@@ -58,6 +63,8 @@ function App() {
     setGridValues(newGridValues);
   }
 
+  /* Handles input through the text bar
+   * Updates table/grid values with new groupings */
   function onMintermInput(minterms) {
     setSelectedMintermGroup(0);
     setActiveMinterms(minterms);
@@ -71,18 +78,18 @@ function App() {
     setGridValues(newGridValues);
   }
 
+  /* Handles button click when table is clicked
+   * Creates a new copy of the values and sets groupings in the copy
+   * Then, the app is updated with the copy */
   function onGridButtonClick(decimalValue) {
     setSelectedMintermGroup(0);
     const gridValuesCopy = new Array(gridValues.length).fill()
       .map((_, idx) => JSON.parse(JSON.stringify(gridValues[idx])));
-    console.log(gridValuesCopy)
-    if (gridValues[decimalValue].value === '0') {
-      gridValuesCopy[decimalValue].value = '1';
-    } else if (gridValues[decimalValue].value === '1') {
-      gridValuesCopy[decimalValue].value = 'X';
-    } else {
-      gridValuesCopy[decimalValue].value = '0';
-    }
+    gridValuesCopy[decimalValue].value = (() => {
+      if (gridValues[decimalValue].value === '0') return '1';
+      if (gridValues[decimalValue].value === '1') return 'X';
+      return '0';
+    })();
     const terms = gridValuesCopy.map((x, idx) => (x.value === '1') ? idx : null).filter(x => x !== null);
     const dontCares = gridValuesCopy.map((x, idx) => (x.value === 'X') ? idx : null).filter(x => x !== null);
     const newActiveMinterms = { terms, dontCares };
@@ -101,7 +108,8 @@ function App() {
     })
   }
 
-  /* Warning: Mutates gridValues and groupings */
+  /* Warning: Mutates gridValues */
+  /* Adds a color array to the grid values depending on the groupings */
   function setupGridColors(gridValues, grouping = []) {
     grouping.forEach((group, groupingIdx) => {
       group.color = tempGroupingColors[groupingIdx];
